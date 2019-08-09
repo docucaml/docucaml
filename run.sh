e42b7da12f38f6f67471bcdc8f9c1cc37d03d6f2
Yotam Barnoy <yotambarnoy@gmail.com>
2019-08-09 16:02:51 -0400

    Limit opam to listing packages compatible with version of compiler

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Script to install all packages in OPAM using esy, then create documentation
-OCAML_VER=4.8.0
+OCAML_VER=4.08.1
 ESY_DIR=$HOME/.esy/3/b
 CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -9,7 +9,7 @@ mkdir ~/packages
 cd ~/packages
 
 # Get the list of available opam packages
-PACKAGES=$(opam list -a | awk '{print $1}')
+PACKAGES=$(opam list -a --coinstallable-with=ocaml.$OCAML_VER | awk '{print $1}')
 for package in $PACKAGES; do
   echo Installing $package
   mkdir ${package}_inst
